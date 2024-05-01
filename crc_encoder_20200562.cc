ac36937453a6b99f384033fa0357ab6839d90588
YOOHOO0 <yoohoo_030@naver.com>
2024-05-01 20:28:53 +0900

    fin

--- crc_encoder_20200562.cc
+++ crc_encoder_20200562.cc
@@ -6,43 +6,42 @@
 using namespace std;
 
 // 이진수 문자열을 XOR 연산으로 계산
-string xor_strings(string& current_portion, string& divisor) {
+string xor_string(string& current, string& divisor) {
     string result;
-    for (int i = 0; i < current_portion.length(); i++) {
+    for (int i = 0; i < current.length(); i++) {
         // XOR 연산
-        result += (current_portion[i] == divisor[i]) ? '0' : '1';
+        result += (current[i] == divisor[i]) ? '0' : '1';
     }
     return result;
 }
 
 // 모듈러-2 나눗셈
-string mod2_division(string& dividend, string& divisor) {
-    // 나눗셈을 위해 이진수 복사
+string mod2(string& dividend, string& divisor) {
+	// remaindero에 dividend 할당하고 시작
     string remainder = dividend;
-    int divisor_len = divisor.length();
 
-    // 나머지에서 앞부분의 가장 왼쪽 비트가 '1'인 경우만 나눗셈 진행
-    while (remainder.length() >= divisor_len) {
-        // 나머지에서 앞부분 자르기
-    	string current_portion = remainder.substr(0, divisor_len);
+    // divisor_len보다 remainder가 작아지면 루프 탈출
+    while (remainder.length() >= divisor.length()) {
+        // remainder에서 앞부분 자르기
+    	string current = remainder.substr(0, divisor.length());
 
-        // XOR 연산을 통해 나누기
-        if (current_portion[0] == '1') { // 나누기 조건
-            current_portion = xor_strings(current_portion, divisor);
+        // 1 X X X일 경우에만 나눗셈 진행
+        if (current[0] == '1') {
+            current = xor_string(current, divisor);
         }
 
         // 나머지 업데이트
-        remainder = current_portion.substr(1) + remainder.substr(divisor_len);
+        remainder = current.substr(1) + remainder.substr(divisor.length());
     }
 
     return remainder; // 나머지 반환
 }
 
-
 int main(int argc, char *argv[]) {
 	ifstream input_file(argv[1]);
 	ofstream output_file(argv[2]);
 
+	// 예외처리
 	if (argc != 5) {
 		cout << "usage: ./crc_encoder input_file output_file generator dataword_size" << endl;
 		return (1);
@@ -60,48 +59,41 @@ int main(int argc, char *argv[]) {
 		return (1);
 	}
 
-	string binaryText;
+	// input_file의 text를 2진수 string dataword으로 변환
+	string dataword;
 	char c;
 	while(input_file.get(c)) {
 		bitset<8> binary(c);
-		binaryText += binary.to_string();
+		dataword += binary.to_string();
 	}
+
+	// 2진수 string dataword를 2진수 string codeword로 변환
 	string generator = argv[3];
 	int dsize = atoi(argv[4]);
-	// dataword가 8일때는 그냥 패딩을 붙이면 되지만 4일 때는 두 결과를 합친 후 앞에 패딩을 붙여야함
-	string resword;
-	for(int i = 0; i < binaryText.length(); i += dsize) {
-		string dataword = binaryText.substr(i, dsize);
+	string codeword;
+	for(int i = 0; i < dataword.length(); i += dsize) {
+		string eachDataword = dataword.substr(i, dsize);
 		string zeros(generator.length() - 1, '0');
-		string tempword = dataword + zeros;
-		string codeword = dataword + mod2_division(tempword, generator);
-		if (dsize == 4) {
-			if (i % 8 == 0) {
-				resword = codeword;
-			}
-			else {
-				resword += codeword;
-				if (resword.length() % 8 != 0) {
-					int padding_num = 8 - (resword.length() % 8);
-					bitset<8> binary(padding_num);
-					string zeros(padding_num, '0');
-					resword = binary.to_string() + zeros + resword;
-				}
-				bitset<8> binary(resword);
-				string resstr = reinterpret_cast<char*>(binary.to_ulong());
-				output_file.write(resstr.c_str(), resstr.length());
-				//output_file.write(resword.c_str(), resword.length());
-			}
-		}
-		else {
-			resword = codeword;
-			if (resword.length() % 8 != 0) {
-					int padding_num = 8 - (resword.length() % 8);
-					bitset<8> binary(padding_num);
-					string zeros(padding_num, '0');
-					resword = binary.to_string() + zeros + resword;
-				}
-			output_file.write(resword.c_str(), resword.length());
-		}
+		string tempword = eachDataword + zeros; // 각각의 dataword 뒤에 0들을 붙여서 모듈러연산 준비
+		string eachCodeword = eachDataword + mod2(tempword, generator); // 모듈러연산으로 codeword 구하기
+		codeword += eachCodeword;
 	}
+
+	// codeword가 8로 나누어 떨어지게끔 codeword 앞에 1byte짜리 패딩 정보와 패딩 비트를 추가함
+	int padding_num;
+	padding_num = !(codeword.length() % 8) ? 0 : 8 - (codeword.length() % 8); // codeword가 8로 나누어떨어지면 패딩을 추가하지 않음
+	bitset<8> binary(padding_num);
+	string zeros(padding_num, '0');
+	codeword = binary.to_string() + zeros + codeword;
+
+	//2진수 string codeword를 8글자당 1byte로 압축
+	string encoded;
+	for(int i = 0; i < codeword.length(); i += 8) {
+		bitset<8> bits(codeword.substr(i, i + 8));
+		char c = static_cast<char>(bits.to_ulong());
+		encoded += c;
+	}
+
+	// write
+	output_file.write(encoded.c_str(), encoded.length());
 }
